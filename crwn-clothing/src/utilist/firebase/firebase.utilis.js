6fe3da334a745eab7fa46a8a2d2e0d58c69340c3
mohyidinhawary <mohyidin2022@gmail.com>
2023-10-18 16:58:06 +0300

    updating our home page

---
+++ crwn-clothing/src/utilist/firebase/firebase.utilis.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXY7q15EskZIgb7sEoslKdGTV4hJ6KxKI",
+  authDomain: "crwn-clothing-db-c766f.firebaseapp.com",
+  projectId: "crwn-clothing-db-c766f",
+  storageBucket: "crwn-clothing-db-c766f.appspot.com",
+  messagingSenderId: "1097489925725",
+  appId: "1:1097489925725:web:1d41ece0c98673dd626fb4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
