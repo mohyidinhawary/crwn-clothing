b7c570805660df7df560960c77d3fba3fab03b14
mohyidinhawary <mohyidin2022@gmail.com>
2023-10-19 17:08:26 +0300

    updating our utilis&signin page

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,16 @@
+import { signInWithGooglePopup,createUserProfileDocument } from "../../utilist/firebase/firebase.utilis"
 const SignIn=()=>{
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+        createUserProfileDocument(response);
+      };
     return(
-        <div>
-            Hello I am SignIn Page
-        </div>
+        
+             <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+        
     )
 }
 export default SignIn
