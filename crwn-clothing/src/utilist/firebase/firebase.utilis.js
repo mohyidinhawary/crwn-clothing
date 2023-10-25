92d17df40a0556fe995e74b542c70695c54be6ae
mohyidinhawary <mohyidin2022@gmail.com>
2023-10-25 17:02:43 +0300

    updating our utilist

--- crwn-clothing/src/utilist/firebase/firebase.utilis.js
+++ crwn-clothing/src/utilist/firebase/firebase.utilis.js
@@ -7,7 +7,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { getFirestore, doc, getDoc, setDoc,collection,writeBatch,query,getDocs } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -39,6 +39,33 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
 export const db = getFirestore();
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+  
+  objectsToAdd.forEach((object) => {
+     const docRef = doc(collectionRef, object.title.toLowerCase());
+     batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+};
+
+export const getCategoriesAndDocuments=async()=>{
+  const  collectionRef=collection(db,'categories'); 
+  const q=query(collectionRef);
+  const querysnapshot=await getDocs(q);
+  const categoryMap=querysnapshot.docs.reduce((acc,docsnapshot)=>{
+    const{title,items}=docsnapshot.data();
+acc[title.toLowerCase()]=items;
+return acc;
+  },{});
+  return categoryMap;
+}
 export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation = {}
